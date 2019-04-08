39b59edfad3cafdc459ed25ce07162b3e5ac3477
Mario Perez <mario.perez@gruposese.com>
2019-04-08 15:57:47 +0200

    Context

--- Ejercito/DAL/EjercitoContext.cs
+++ Ejercito/DAL/EjercitoContext.cs
@@ -15,22 +15,23 @@ namespace Ejercito.DAL
         {
 
         }
-        public DbSet<Canon> Canones { get; set; }
         public DbSet<Division> Divisiones { get; set; }
         public DbSet<CapituloEjercito> Ejercitos { get; set; }
+
+
+        public DbSet<Ametrallador> Ametralladors { get; set; }
+        public DbSet<AntiAereo> AntiAereos { get; set; }
+        public DbSet<Canon> Canones { get; set; }
+        public DbSet<InfanteriaBasica> InfanteriaBasicas { get; set; }
+        public DbSet<MX7899> MX7899s { get; set; }
+        public DbSet<Sanitario> Sanitarios { get; set; }
+        public DbSet<TAXIN66> TAXIN66s { get; set; }
+        public DbSet<TorpederoM> TorpederoMs { get; set; }
+        public DbSet<VB98> VB98s { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
-
-        public System.Data.Entity.DbSet<Ejercito.Models.Unidades.Ametrallador> Ametralladors { get; set; }
-
-        public System.Data.Entity.DbSet<Ejercito.Models.Unidades.AntiAereo> AntiAereos { get; set; }
-
-        public System.Data.Entity.DbSet<Ejercito.Models.Unidades.InfanteriaBasica> InfanteriaBasicas { get; set; }
-
-        public System.Data.Entity.DbSet<Ejercito.Models.Unidades.MX7899> MX7899 { get; set; }
-
-        public System.Data.Entity.DbSet<Ejercito.Models.Unidades.TAXIN66> TAXIN66 { get; set; }
     }
 }
