b033b4d416361863a81d7e5b623cd5fd03189de9
Mario Perez <mario.perez@gruposese.com>
2019-04-08 15:40:08 +0200

    sanitario

--- Ejercito/Models/Unidades/Sanitario.cs
+++ Ejercito/Models/Unidades/Sanitario.cs
@@ -5,7 +5,29 @@ using System.Web;
 
 namespace Ejercito.Models.Unidades
 {
-    public class Sanitario :
+    public class Sanitario : IUnidad, IBlindado, IMovimiento
     {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public Division Division { get; set; }
+        public int Valor { get; set; }
+        public double Blindaje { get; set; }
+        public double Movimiento { get; set; }
+
+        public double DameBlindaje()
+        {
+            return Blindaje;
+        }
+
+        public double DameMovimiento()
+        {
+            return Movimiento;
+        }
+
+        public int DameValor()
+        {
+            return Valor;
+        }
     }
 }
